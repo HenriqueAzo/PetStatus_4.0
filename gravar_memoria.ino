ba1b82b89ab2dcff4c4308d5712a33a3ec28afc9
Henrique Azoubel <84883028+HenriqueAzo@users.noreply.github.com>
2023-05-01 11:36:02 -0300

    Create gravar_memoria.ino
    pequeno código para gravar manualmente na memória de um arduino.

---
+++ gravar_memoria.ino
@@ -0,0 +1,22 @@
+#include <EEPROM.h>
+
+//verificar antes de gravar
+int senha[6] = {0, 5, 0, 9, 1, 9}; // Cria um array de 5 inteiros com valores diferentes
+int user = 2;
+int iteration = 6*2;
+void setup() {
+  Serial.begin(9600);
+  
+
+  for(int i = 0; i <6; i++) {
+    EEPROM.write(i+(user*6), senha[i]);
+    Serial.print("Escrevendo o dígito ");
+    Serial.print(senha[i]);
+    Serial.print(" no endereço ");
+    Serial.println(i+(user*6));
+  }
+}
+
+void loop() {
+
+}
